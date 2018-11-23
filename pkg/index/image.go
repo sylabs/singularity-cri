79ac200953bf3652da679223efb23a86383ebc2d
sashayakovtseva <sashayakovtseva@gmail.com>
2018-11-23 15:34:04 +0300

    Handle updated image upon pull correctly
    Fixes #81

--- pkg/index/image.go
+++ pkg/index/image.go
@@ -58,6 +58,43 @@ func (i *ImageIndex) Find(id string) (*image.Info, error) {
 	return info, err
 }
 
+// Add adds the given image info to the index.
+// If image with the same ID already exists it updates old image info appropriately.
+func (i *ImageIndex) Add(image *image.Info) error {
+	oldImage, err := i.Find(image.ID())
+	if err != nil && err != ErrImageNotFound {
+		return fmt.Errorf("could not find old image: %v", err)
+	}
+	if err == ErrImageNotFound {
+		return i.add(image)
+	}
+	return i.merge(oldImage, image)
+}
+
+// Remove removes pod from index if it present or returns otherwise.
+func (i *ImageIndex) Remove(id string) error {
+	imgInfo, err := i.Find(id)
+	if err != nil {
+		return err
+	}
+	err = i.indx.Delete(imgInfo.ID())
+	if err != nil {
+		return fmt.Errorf("could not remove image: %v", err)
+	}
+
+	i.removeRefs(imgInfo.Ref().Tags()...)
+	i.removeRefs(imgInfo.Ref().Digests()...)
+	return nil
+}
+
+// Iterate calls handler func on each pod registered in index.
+func (i *ImageIndex) Iterate(handler func(image *image.Info)) {
+	innerIterate := func(key string, item interface{}) {
+		handler(item.(*image.Info))
+	}
+	i.indx.Iterate(innerIterate)
+}
+
 func (i *ImageIndex) find(id string) (*image.Info, error) {
 	item, err := i.indx.Get(id)
 	if err == truncindex.ErrNotFound {
@@ -70,36 +107,10 @@ func (i *ImageIndex) find(id string) (*image.Info, error) {
 	return info, nil
 }
 
-// Remove removes pod from index if it present or returns otherwise.
-func (i *ImageIndex) Remove(id string) error {
-	imgInfo, err := i.Find(id)
-	if err != nil {
-		return err
-	}
-	err = i.indx.Delete(imgInfo.ID())
-	if err != nil {
-		return fmt.Errorf("could not remove imgInfo: %v", err)
-	}
-
-	i.removeRefs(imgInfo.Ref().Tags()...)
-	i.removeRefs(imgInfo.Ref().Digests()...)
-	return nil
-}
-
-// Add adds the given expectImage info. If expectImage already exists it
-// updates old info appropriately.
-func (i *ImageIndex) Add(image *image.Info) error {
-	oldImage, err := i.Find(image.ID())
-	if err != nil && err != ErrImageNotFound {
-		return fmt.Errorf("could not find old image: %v", err)
-	}
-	if err == ErrImageNotFound {
-		return i.add(image)
-	}
-	return i.merge(oldImage, image)
-}
-
 func (i *ImageIndex) add(image *image.Info) error {
+	if err := i.removeOverlapRefs(image); err != nil {
+		return nil
+	}
 	err := i.indx.Add(image.ID(), image)
 	if err != nil {
 		return fmt.Errorf("could not add image: %v", err)
@@ -119,31 +130,45 @@ func (i *ImageIndex) merge(oldImage, image *image.Info) error {
 
 	for _, tag := range image.Ref().Tags() {
 		oldID := i.readRef(tag)
-		if oldID == image.ID() {
-			continue
-		}
-		oldInfo, _ := i.Find(image.ID())
-		oldInfo.Ref().RemoveTag(tag)
 		i.setRef(tag, image.ID())
+		if oldID != "" && oldID != image.ID() {
+			oldInfo, _ := i.Find(image.ID())
+			oldInfo.Ref().RemoveTag(tag)
+		}
 	}
 	for _, digest := range image.Ref().Digests() {
 		oldID := i.readRef(digest)
-		if oldID == image.ID() {
-			continue
-		}
-		oldInfo, _ := i.Find(image.ID())
-		oldInfo.Ref().RemoveDigest(digest)
 		i.setRef(digest, image.ID())
+		if oldID != "" && oldID == image.ID() {
+			oldInfo, _ := i.Find(image.ID())
+			oldInfo.Ref().RemoveDigest(digest)
+		}
 	}
 	return nil
 }
 
-// Iterate calls handler func on each pod registered in index.
-func (i *ImageIndex) Iterate(handler func(image *image.Info)) {
-	innerIterate := func(key string, item interface{}) {
-		handler(item.(*image.Info))
+func (i *ImageIndex) removeOverlapRefs(image *image.Info) error {
+	for _, tag := range image.Ref().Tags() {
+		oldID := i.readRef(tag)
+		if oldID != "" {
+			oldImg, err := i.find(oldID)
+			if err != nil {
+				return err
+			}
+			oldImg.Ref().RemoveTag(tag)
+		}
 	}
-	i.indx.Iterate(innerIterate)
+	for _, digest := range image.Ref().Digests() {
+		oldID := i.readRef(digest)
+		if oldID != "" {
+			oldImg, err := i.find(oldID)
+			if err != nil {
+				return err
+			}
+			oldImg.Ref().RemoveDigest(digest)
+		}
+	}
+	return nil
 }
 
 func (i *ImageIndex) readRef(ref string) string {
