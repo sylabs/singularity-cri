6f95d79268a385ca3c537703d5ae03a479ac2738
Sasha Yakovtseva <sashayakovtseva@gmail.com>
2019-02-05 23:10:04 +0300

    Return import corrupted after merge

--- pkg/server/runtime/runtime.go
+++ pkg/server/runtime/runtime.go
@@ -26,6 +26,7 @@ import (
 	"github.com/sylabs/singularity-cri/pkg/kube"
 	"github.com/sylabs/singularity-cri/pkg/network"
 	"github.com/sylabs/singularity-cri/pkg/singularity"
+	snetwork "github.com/sylabs/singularity/pkg/network"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	k8s "k8s.io/kubernetes/pkg/kubelet/apis/cri/runtime/v1alpha2"
